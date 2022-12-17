a7a74dea09be610a12c099d9eb16867fb2204fe8
Sabbir Ahmed Shubo <sabbir.ove@gmail.com>
2022-12-17 12:08:20 +0600

    Add custom csv formmatter for company

--- CompanyEmployees/Extentions/ServiceExtentions.cs
+++ CompanyEmployees/Extentions/ServiceExtentions.cs
@@ -1,4 +1,5 @@
-﻿using Contracts.Logger;
+﻿using CompanyEmployees.ResponseFormatter;
+using Contracts.Logger;
 using Contracts.Repository;
 using LoggerService;
 using Microsoft.EntityFrameworkCore;
@@ -9,9 +10,17 @@ namespace CompanyEmployees.Extentions
 {
     public static class ServiceExtentions
     {
-        public static void ConfigureLoggerService(this IServiceCollection services) => services.AddScoped<ILoggerManager, LoggerManager>();
-        public static void ConfigureRepositoryService(this IServiceCollection services) => services.AddScoped<IRepositoryManager, RepositoryManager>();
+        public static void ConfigureLoggerService(this IServiceCollection services) =>
+            services.AddScoped<ILoggerManager, LoggerManager>();
+        public static void ConfigureRepositoryService(this IServiceCollection services) => 
+            services.AddScoped<IRepositoryManager, RepositoryManager>();
 
-        public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration) => services.AddDbContext<RepositoryContext>(opts => opts.UseSqlServer(configuration.GetConnectionString("sqlConnection")));
+        public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration) => 
+            services.AddDbContext<RepositoryContext>(opts => 
+            opts.UseSqlServer(configuration.GetConnectionString("sqlConnection")));
+
+        public static IMvcBuilder AddCustomCSVFormatter(this IMvcBuilder builder) => 
+            builder.AddMvcOptions(config => 
+            config.OutputFormatters.Add(new CsvOutputFormatter()));
     }
 }
--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -13,7 +13,14 @@ builder.Services.ConfigureSqlContext(builder.Configuration);
 builder.Services.AddAutoMapper(typeof(Program));
 builder.Services.ConfigureRepositoryService();
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(config =>
+{
+    config.RespectBrowserAcceptHeader = true;
+    config.ReturnHttpNotAcceptable = true;
+}).
+AddXmlDataContractSerializerFormatters().
+AddCustomCSVFormatter();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
