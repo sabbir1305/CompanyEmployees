b49c76bb9f53c995116f0f93ccff47264ab1d384
Sabbir Ahmed Shubo <sabbir.ove@gmail.com>
2023-01-12 22:57:27 +0600

    Add header and options

--- CompanyEmployees/Controllers/CompaniesController.cs
+++ CompanyEmployees/Controllers/CompaniesController.cs
@@ -124,5 +124,12 @@ namespace CompanyEmployees.Controllers
             await _repository.SaveAsync();
             return NoContent();
         }
+
+        [HttpOptions]
+        public IActionResult GetCompaniesOptions()
+        {
+            Response.Headers.Add("Allow", "GET, OPTIONS, POST");
+            return Ok();
+        }
     }
 }
